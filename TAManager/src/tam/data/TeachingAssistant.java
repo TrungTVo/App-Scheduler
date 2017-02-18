1c0c53abf853f57268a192acf435b3457249803c
Trung Vo <vtt311096@gmail.com>
2017-02-18 00:03:30 -0500

    Initial commit

---
+++ TAManager/src/tam/data/TeachingAssistant.java
@@ -0,0 +1,41 @@
+package tam.data;
+
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+/**
+ * This class represents a Teaching Assistant for the table of TAs.
+ * 
+ * @author Richard McKenna
+ */
+public class TeachingAssistant<E extends Comparable<E>> implements Comparable<E>  {
+    // THE TABLE WILL STORE TA NAMES AND EMAILS
+    private final StringProperty name;
+
+    /**
+     * Constructor initializes the TA name
+     */
+    public TeachingAssistant(String initName) {
+        name = new SimpleStringProperty(initName);
+    }
+
+    // ACCESSORS AND MUTATORS FOR THE PROPERTIES
+
+    public String getName() {
+        return name.get();
+    }
+
+    public void setName(String initName) {
+        name.set(initName);
+    }
+
+    @Override
+    public int compareTo(E otherTA) {
+        return getName().compareTo(((TeachingAssistant)otherTA).getName());
+    }
+    
+    @Override
+    public String toString() {
+        return name.getValue();
+    }
+}
